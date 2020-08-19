e8eb8d1208f3dd69bec8d644131a9debdc3d9185
SPBravo <jpontebravo@gmail.com>
2020-08-18 21:28:29 -0400

    home page sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/reusables/navbar/navbar.component';
+import { FooterComponent } from './components/reusables/footer/footer.component';
+import { ImageSidebarComponent } from './components/home-components/image-sidebar/image-sidebar.component';
+import { HomeContentComponent } from './components/home-components/home-content/home-content.component';
+import { AboutComponent } from './components/home-components/about/about.component';
+import { ExperienceComponent } from './components/home-components/experience/experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    ImageSidebarComponent,
+    HomeContentComponent,
+    AboutComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule,
